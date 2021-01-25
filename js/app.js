c2fdf3d708bdc64f94f6610cadd080b4089fca1b
iamniama <plectrumonthespectrum@gmail.com>
2021-01-25 11:41:54 -0700

    Game class now adjust question point value based on time thresholds

--- js/app.js
+++ js/app.js
@@ -6,10 +6,16 @@ class Game {
         this.currentRoundTimer = intRoundDuration;
         this.players = this.addPlayers(intPlayers);
         this.watchTimer = false;
-        this.timer = setInterval(this.tick.bind(this), 1000)
+        this.timer = setInterval(this.tick.bind(this), 1000);
+        this.pointValue = 250;
+        this.timeThreshold1 = 11;
+        this.timeThreshold2 = 6;
     }
     addPlayers(intPlayers){
         console.log(`There will be ${intPlayers} players in this game`)
+    }
+    startRound(){
+        this.currentRoundTimer = this.roundDuration
     }
     tick(){
         //console.log("Timer works...")
@@ -18,6 +24,7 @@ class Game {
             console.log(`Round ${this.currentRound} ends...`)
             this.currentRoundTimer = this.roundDuration
             this.currentRound += 1
+            this.pointValue = 250
             if (this.currentRound <= this.rounds) {
                 console.log(`Round ${this.currentRound} begins...`)
             }else {
@@ -28,12 +35,16 @@ class Game {
         }
         if (this.currentRoundTimer > 0 && this.watchTimer){
             this.currentRoundTimer -= 1
-            console.log(`${this.currentRoundTimer} seconds remain in round ${this.currentRound}`)
+            console.log(`${this.currentRoundTimer} seconds remain in round ${this.currentRound}, question is worth ${this.pointValue} points`)
 
+        }
+        if (this.currentRoundTimer == this.timeThreshold1 || this.currentRoundTimer == this.timeThreshold2){
+            this.pointValue -= 50
         }
     }
     start(){
         this.watchTimer = true
+        this.pointValue = 250
         console.log(`new game starting...${this.watchTimer}`)
     }
     stop(){
