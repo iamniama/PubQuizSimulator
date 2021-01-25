500850f922e9b9f6cff8369a91ae31bc319b1fd7
iamniama <plectrumonthespectrum@gmail.com>
2021-01-25 11:31:37 -0700

    initial structure of Game class with working timer via setInterval

--- js/app.js
+++ js/app.js
@@ -6,17 +6,25 @@ class Game {
         this.currentRoundTimer = intRoundDuration;
         this.players = this.addPlayers(intPlayers);
         this.watchTimer = false;
-        this.timer = setInterval(this.tick, 1000)
+        this.timer = setInterval(this.tick.bind(this), 1000)
     }
     addPlayers(intPlayers){
         console.log(`There will be ${intPlayers} players in this game`)
     }
     tick(){
+        //console.log("Timer works...")
+        //console.log(`watchTimer is ${this.watchTimer}, Current Round Timer: ${this.currentRoundTimer}`)
         if (this.watchTimer && this.currentRoundTimer == 0){
             console.log(`Round ${this.currentRound} ends...`)
             this.currentRoundTimer = this.roundDuration
             this.currentRound += 1
-            console.log(`Round ${this.currentRound} begins...`)
+            if (this.currentRound <= this.rounds) {
+                console.log(`Round ${this.currentRound} begins...`)
+            }else {
+                console.log("Game over")
+                this.watchTimer = false
+                this.end()
+            }
         }
         if (this.currentRoundTimer > 0 && this.watchTimer){
             this.currentRoundTimer -= 1
@@ -26,9 +34,13 @@ class Game {
     }
     start(){
         this.watchTimer = true
+        console.log(`new game starting...${this.watchTimer}`)
     }
     stop(){
         this.watchTimer = false
+    }
+    end(){
+        clearInterval(this.timer)
     }
 }
 
